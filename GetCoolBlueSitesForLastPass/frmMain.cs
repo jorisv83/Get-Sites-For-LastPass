5232f723bb56431eb434e273d8954dc0e21d378a
Joris <joris.onghena@ua.ac.be>
2013-01-25 14:20:02 +0100

    Added comments and did some code cleanup

--- GetCoolBlueSitesForLastPass/frmMain.cs
+++ GetCoolBlueSitesForLastPass/frmMain.cs
@@ -8,10 +8,18 @@ namespace GetCoolBlueSitesForLastPass
 {
     public partial class frmMain : Form
     {
+        /// -------------------------------------------
+        /// Constructor
+        /// -------------------------------------------
+
         public frmMain()
         {
             InitializeComponent();
         }
+
+        /// -------------------------------------------
+        /// Form events
+        /// -------------------------------------------
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -20,7 +28,7 @@ namespace GetCoolBlueSitesForLastPass
 
         private void btnGetURLS_Click(object sender, EventArgs e)
         {
-            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            using (WebClient client = new WebClient())
             {
                 string strHtmlCode = client.DownloadString(this.txtCoolBlueWebsiteURL.Text);
                 List<string> listFoundUrls = new List<string>();
@@ -47,21 +55,37 @@ namespace GetCoolBlueSitesForLastPass
                     this.txtResult.Text += string.Concat(s.Trim(), ", ");
                 }
 
-                if (!listFoundUrls.Contains(this.txtCoolBlueWebsiteURL.Text.Replace("http://www.", "").Replace("http://", "").Replace(",", "").Trim()))
-                    this.txtResult.Text = string.Concat(this.txtCoolBlueWebsiteURL.Text.Replace("http://www.", "").Replace("http://", "").Replace(",", "").Trim(), ", ", this.txtResult.Text);
+                if (!listFoundUrls.Contains(this.CleanURL(this.txtCoolBlueWebsiteURL.Text)))
+                    this.txtResult.Text = string.Concat(this.CleanURL(this.txtCoolBlueWebsiteURL.Text), ", ", this.txtResult.Text);
 
                 if (this.txtResult.Text.EndsWith(", "))
                     this.txtResult.Text = this.txtResult.Text.Trim().Trim(',').Trim();
             }
         }
 
+        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.txtResult.Text))
+                Clipboard.SetText(this.txtResult.Text);
+            else
+                MessageBox.Show("Nothing to paste to the clipboard...", "Nothing to paste", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        // -------------------------------------------
+        // Private methods
+        // -------------------------------------------
+
+        /// <summary>
+        /// Method to download all the sites from the given HTML source
+        /// </summary>
+        /// <param name="strHtmlCode">The HTML source</param>
+        /// <param name="listFoundUrls">The list of found URL's</param>
         private void GetSites(string strHtmlCode, ref List<string> listFoundUrls)
         {
             string strRegex1 = ".*<a href=\"(.*)\" class=\"nopopup coolbluebar_shopcategory_shop_link\" title=\"(.*)\">.*";
             string strRegex11 = ".*<a href=\"(.*)\" class=\"coolbluebar_shopcategory_shop_link display_block outline_none decoration_none\".*";
             string strRegex2 = "a\\[.*=\".*\"\\],";
             string strFoundUrl = "";
-
 
             Regex urlRegex = new Regex(strRegex1);
             MatchCollection mc = urlRegex.Matches(strHtmlCode);
@@ -70,7 +94,7 @@ namespace GetCoolBlueSitesForLastPass
             {
                 if (m.Groups.Count == 3)
                 {
-                    strFoundUrl = m.Groups[1].Value.ToLower().Replace("http://www.", "").Replace("http://", "").Replace(",", "").Trim();
+                    strFoundUrl = this.CleanURL(m.Groups[1].Value);
                     if (!listFoundUrls.Contains(strFoundUrl))
                         listFoundUrls.Add(strFoundUrl);
                 }
@@ -83,7 +107,7 @@ namespace GetCoolBlueSitesForLastPass
             {
                 if (m.Groups.Count == 2)
                 {
-                    strFoundUrl = m.Groups[1].Value.ToLower().Replace("http://www.", "").Replace("http://", "").Replace(",", "").Trim();
+                    strFoundUrl = this.CleanURL(m.Groups[1].Value);
                     if (!listFoundUrls.Contains(strFoundUrl))
                         listFoundUrls.Add(strFoundUrl);
                 }
@@ -97,20 +121,27 @@ namespace GetCoolBlueSitesForLastPass
                 string[] strArrSplit = strFoundUrl.Split(new string[] { ", a[href*=\"" }, StringSplitOptions.RemoveEmptyEntries);
                 foreach (string s in strArrSplit)
                 {
-                    strFoundUrl = s.Replace("a[href*=\"", "").Replace("\"]", "");
-                    strFoundUrl = strFoundUrl.ToLower().Replace("http://www.", "").Replace("http://", "").Replace(",", "").Trim();
+                    strFoundUrl = this.CleanURL(s);
                     if (!listFoundUrls.Contains(strFoundUrl))
                         listFoundUrls.Add(strFoundUrl);
                 }
             }
         }
 
-        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Method to clean a URL string to use with lastpass
+        /// </summary>
+        /// <param name="strUrl">The URL string</param>
+        /// <returns>A cleaned URL</returns>
+        private string CleanURL(string strUrl)
         {
-            if (!string.IsNullOrEmpty(this.txtResult.Text))
-                Clipboard.SetText(this.txtResult.Text);
-            else
-                MessageBox.Show("Nothing to paste to the clipboard...", "Nothing to paste", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return strUrl.ToLower().Trim()
+                .Replace("a[href*=\"", "")
+                .Replace("\"]", "")
+                .Replace("http://www.", "")
+                .Replace("http://", "")
+                .Replace(",", "")
+                .Trim();
         }
 
     }
