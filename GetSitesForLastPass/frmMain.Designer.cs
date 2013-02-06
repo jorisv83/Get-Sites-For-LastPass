1dc983b3ff7eaeff62f3078f669e86e5da4f4edf
Joris <joris.onghena@ua.ac.be>
2013-02-06 15:31:53 +0100

    Renames solution; Added support for other sites;

---
+++ GetSitesForLastPass/frmMain.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace GetSitesForLastPass
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.grpBoxResult = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnGetURLS = new System.Windows.Forms.Button();
+            this.lblCoolBlueWebsiteURL = new System.Windows.Forms.Label();
+            this.btnCopyToClipboard = new System.Windows.Forms.Button();
+            this.cbWebsiteURL = new System.Windows.Forms.ComboBox();
+            this.grpBoxSource = new System.Windows.Forms.GroupBox();
+            this.grpBoxControl = new System.Windows.Forms.GroupBox();
+            this.grpBoxResult.SuspendLayout();
+            this.grpBoxSource.SuspendLayout();
+            this.grpBoxControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpBoxResult
+            // 
+            this.grpBoxResult.Controls.Add(this.txtResult);
+            this.grpBoxResult.Location = new System.Drawing.Point(193, 12);
+            this.grpBoxResult.Name = "grpBoxResult";
+            this.grpBoxResult.Size = new System.Drawing.Size(253, 203);
+            this.grpBoxResult.TabIndex = 0;
+            this.grpBoxResult.TabStop = false;
+            this.grpBoxResult.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(6, 20);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(241, 177);
+            this.txtResult.TabIndex = 0;
+            // 
+            // btnGetURLS
+            // 
+            this.btnGetURLS.Location = new System.Drawing.Point(9, 19);
+            this.btnGetURLS.Name = "btnGetURLS";
+            this.btnGetURLS.Size = new System.Drawing.Size(155, 37);
+            this.btnGetURLS.TabIndex = 1;
+            this.btnGetURLS.Text = "Go !!!";
+            this.btnGetURLS.UseVisualStyleBackColor = true;
+            this.btnGetURLS.Click += new System.EventHandler(this.btnGetURLS_Click);
+            // 
+            // lblCoolBlueWebsiteURL
+            // 
+            this.lblCoolBlueWebsiteURL.AutoSize = true;
+            this.lblCoolBlueWebsiteURL.Location = new System.Drawing.Point(6, 24);
+            this.lblCoolBlueWebsiteURL.Name = "lblCoolBlueWebsiteURL";
+            this.lblCoolBlueWebsiteURL.Size = new System.Drawing.Size(76, 13);
+            this.lblCoolBlueWebsiteURL.TabIndex = 2;
+            this.lblCoolBlueWebsiteURL.Text = "Select website";
+            // 
+            // btnCopyToClipboard
+            // 
+            this.btnCopyToClipboard.Location = new System.Drawing.Point(9, 65);
+            this.btnCopyToClipboard.Name = "btnCopyToClipboard";
+            this.btnCopyToClipboard.Size = new System.Drawing.Size(155, 37);
+            this.btnCopyToClipboard.TabIndex = 4;
+            this.btnCopyToClipboard.Text = "Copy result to clipboard";
+            this.btnCopyToClipboard.UseVisualStyleBackColor = true;
+            this.btnCopyToClipboard.Click += new System.EventHandler(this.btnCopyToClipboard_Click);
+            // 
+            // cbWebsiteURL
+            // 
+            this.cbWebsiteURL.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbWebsiteURL.FormattingEnabled = true;
+            this.cbWebsiteURL.Items.AddRange(new object[] {
+            "http://www.coolblue.be",
+            "http://opendesktop.org",
+            "http://fallout3.nexusmods.com"});
+            this.cbWebsiteURL.Location = new System.Drawing.Point(9, 40);
+            this.cbWebsiteURL.Name = "cbWebsiteURL";
+            this.cbWebsiteURL.Size = new System.Drawing.Size(155, 21);
+            this.cbWebsiteURL.TabIndex = 5;
+            // 
+            // grpBoxSource
+            // 
+            this.grpBoxSource.Controls.Add(this.lblCoolBlueWebsiteURL);
+            this.grpBoxSource.Controls.Add(this.cbWebsiteURL);
+            this.grpBoxSource.Location = new System.Drawing.Point(12, 12);
+            this.grpBoxSource.Name = "grpBoxSource";
+            this.grpBoxSource.Size = new System.Drawing.Size(175, 79);
+            this.grpBoxSource.TabIndex = 6;
+            this.grpBoxSource.TabStop = false;
+            this.grpBoxSource.Text = "Source";
+            // 
+            // grpBoxControl
+            // 
+            this.grpBoxControl.Controls.Add(this.btnGetURLS);
+            this.grpBoxControl.Controls.Add(this.btnCopyToClipboard);
+            this.grpBoxControl.Location = new System.Drawing.Point(12, 97);
+            this.grpBoxControl.Name = "grpBoxControl";
+            this.grpBoxControl.Size = new System.Drawing.Size(175, 118);
+            this.grpBoxControl.TabIndex = 7;
+            this.grpBoxControl.TabStop = false;
+            this.grpBoxControl.Text = "Control";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 225);
+            this.Controls.Add(this.grpBoxControl);
+            this.Controls.Add(this.grpBoxSource);
+            this.Controls.Add(this.grpBoxResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Get all domain names";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpBoxResult.ResumeLayout(false);
+            this.grpBoxResult.PerformLayout();
+            this.grpBoxSource.ResumeLayout(false);
+            this.grpBoxSource.PerformLayout();
+            this.grpBoxControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBoxResult;
+        private System.Windows.Forms.Button btnGetURLS;
+        private System.Windows.Forms.Label lblCoolBlueWebsiteURL;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCopyToClipboard;
+        private System.Windows.Forms.ComboBox cbWebsiteURL;
+        private System.Windows.Forms.GroupBox grpBoxSource;
+        private System.Windows.Forms.GroupBox grpBoxControl;
+    }
+}
+
---
+++ GetSitesForLastPass/frmMain.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GetSitesForLastPass
+{
+    public partial class frmMain : Form
+    {
+        /// -------------------------------------------
+        /// Constructor
+        /// -------------------------------------------
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // -------------------------------------------
+        // Form events
+        // -------------------------------------------
+
+        /// <summary>
+        /// Form load event
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cbWebsiteURL.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Get domains button click event
+        /// </summary>
+        private void btnGetURLS_Click(object sender, EventArgs e)
+        {
+            this.grpBoxResult.Text = "Result";
+
+            using (WebClient client = new WebClient())
+            {
+                string strHtmlCode = client.DownloadString(this.cbWebsiteURL.Text);
+                List<string> listFoundUrls = new List<string>();
+                bool bExtra = false;
+
+                ///Check which site is selected...                
+                if (this.cbWebsiteURL.SelectedIndex == 0) //==> Get CoolBlue domains
+                {
+                    GetCoolBlueSites(strHtmlCode, ref listFoundUrls);
+                    //Extra check...                    
+                    foreach (string s in listFoundUrls)
+                    {
+                        if (s == "gsmstore.be")
+                        {
+                            bExtra = true;
+                            strHtmlCode = client.DownloadString(string.Concat("http://www.", s));
+                            break;
+                        }
+                    }
+                    if (bExtra)
+                        GetCoolBlueSites(strHtmlCode, ref listFoundUrls);
+                }
+                else if (this.cbWebsiteURL.SelectedIndex == 1) //==> Get openDesktop domains
+                {
+                    GetOpenDesktopSites(strHtmlCode, ref listFoundUrls);
+                }
+                else if (this.cbWebsiteURL.SelectedIndex == 2) //==> Get nexus domains
+                {
+                    GetNexusSites(strHtmlCode, ref listFoundUrls);
+                }
+                else
+                {
+                    MessageBox.Show("Could not process this site...", "Could not continue", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                WriteResults(ref listFoundUrls);
+            }
+        }
+
+        /// <summary>
+        /// Copy result to clipboard button click event
+        /// </summary>
+        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.txtResult.Text))
+                Clipboard.SetText(this.txtResult.Text);
+            else
+                MessageBox.Show("Nothing to paste to the clipboard...", "Nothing to paste", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        // -------------------------------------------
+        // Private methods
+        // -------------------------------------------
+
+        /// <summary>
+        /// Method to write all the URL's in the list to the results textBox
+        /// </summary>
+        /// <param name="listFoundUrls">The list of found URL's</param>
+        private void WriteResults(ref List<string> listFoundUrls)
+        {
+            this.grpBoxResult.Text = string.Concat("Result - Found ", listFoundUrls.Count, " sites");
+
+            this.txtResult.Text = "";
+
+            listFoundUrls.Sort();
+
+            foreach (string s in listFoundUrls)
+            {
+                this.txtResult.Text += string.Concat(s.Trim(), ", ");
+            }
+
+            if (!listFoundUrls.Contains(this.CleanURL(this.cbWebsiteURL.Text)))
+                this.txtResult.Text = string.Concat(this.CleanURL(this.cbWebsiteURL.Text), ", ", this.txtResult.Text);
+
+            if (this.txtResult.Text.EndsWith(", "))
+                this.txtResult.Text = this.txtResult.Text.Trim().Trim(',').Trim();
+        }
+
+        /// <summary>
+        /// Method to download all the CoolBlue sites from the given HTML source
+        /// </summary>
+        /// <param name="strHtmlCode">The HTML source</param>
+        /// <param name="listFoundUrls">The list of found URL's</param>
+        private void GetCoolBlueSites(string strHtmlCode, ref List<string> listFoundUrls)
+        {
+            string strRegex1 = ".*<a href=\"(.*)\" class=\"nopopup coolbluebar_shopcategory_shop_link\" title=\"(.*)\">.*";
+            string strRegex11 = ".*<a href=\"(.*)\" class=\"coolbluebar_shopcategory_shop_link display_block outline_none decoration_none\".*";
+            string strRegex2 = "a\\[.*=\".*\"\\],";
+            string strFoundUrl = "";
+
+            ExecuteRegexAndAddToList(strHtmlCode, strRegex1, ref listFoundUrls);
+            ExecuteRegexAndAddToList(strHtmlCode, strRegex11, ref listFoundUrls);
+
+            Regex urlRegex = new Regex(strRegex2);
+            MatchCollection mc = urlRegex.Matches(strHtmlCode);
+            foreach (Match m in mc)
+            {
+                strFoundUrl = m.Value;
+                string[] strArrSplit = strFoundUrl.Split(new string[] { ", a[href*=\"" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in strArrSplit)
+                {
+                    strFoundUrl = this.CleanURL(s);
+                    if (!listFoundUrls.Contains(strFoundUrl))
+                        listFoundUrls.Add(strFoundUrl);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get all the domains from the opendesktop website
+        /// </summary>
+        /// <param name="strHtmlCode">The HTML source</param>
+        /// <param name="listFoundUrls">The list of found URL's</param>
+        private void GetOpenDesktopSites(string strHtmlCode, ref List<string> listFoundUrls)
+        {
+            string strRegex1 = "&nbsp;<a href=\"(http.*)\">(.*)</a>.*<br />";
+            string strRegex2 = "<a class=\"maintopnaviopenpc\" href=\"(http.*)\">";
+            string strFoundUrl = "";
+
+            ExecuteRegexAndAddToList(strHtmlCode, strRegex1, ref listFoundUrls);
+
+            Regex urlRegex = new Regex(strRegex1);
+            MatchCollection mc = urlRegex.Matches(strHtmlCode);
+            foreach (Match m in mc)
+            {
+                strFoundUrl = m.Value;
+                string[] strArrSplit = strFoundUrl.Split(new string[] { "&nbsp;&nbsp;&nbsp;" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in strArrSplit)
+                {
+                    ExecuteRegexAndAddToList(s, strRegex2, ref listFoundUrls);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get all the domains from the nexus website
+        /// </summary>
+        /// <param name="strHtmlCode">The HTML source</param>
+        /// <param name="listFoundUrls">The list of found URL's</param>
+        private void GetNexusSites(string strHtmlCode, ref List<string> listFoundUrls)
+        {
+            string strRegex1 = "<li><a href=\"(.*)\">.*</li>";
+            string strRegex2 = ".*<a href=\"(http.*nexusmods.com)\">.*";
+            string strFoundUrl = "";
+
+            Regex urlRegex = new Regex(strRegex1);
+            MatchCollection mc = urlRegex.Matches(strHtmlCode);
+            foreach (Match m in mc)
+            {
+                strFoundUrl = m.Value;
+                string[] strArrSplit = strFoundUrl.Split(new string[] { "</li><li>" }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in strArrSplit)
+                {
+                    ExecuteRegexAndAddToList(s, strRegex2, ref listFoundUrls);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to clean a URL string to use with lastpass
+        /// </summary>
+        /// <param name="strUrl">The URL string</param>
+        /// <returns>A cleaned URL</returns>
+        private string CleanURL(string strUrl)
+        {
+            return strUrl.ToLower().Trim()
+                .Replace("a[href*=\"", "")
+                .Replace("\"]", "")
+                .Replace("http://www.", "")
+                .Replace("http://", "")
+                .Replace("https://www.", "")
+                .Replace("https://", "")
+                .Replace(",", "")
+                .Trim();
+        }
+
+        /// <summary>
+        /// Executes a regular expression and adds the resulting found groups to a given list
+        /// </summary>
+        /// <param name="strHtmlCode">The text to apply the regex on</param>
+        /// <param name="strRegularExpression">The expression</param>
+        /// <param name="listFoundUrls">The list with the results</param>
+        private void ExecuteRegexAndAddToList(string strHtmlCode, string strRegularExpression, ref List<string> listFoundUrls)
+        {
+            Regex urlRegex = new Regex(strRegularExpression);
+            MatchCollection mc = urlRegex.Matches(strHtmlCode);
+            string strFoundUrl;
+
+            foreach (Match m in mc)
+            {
+                /*if (m.Groups.Count == 3)
+                {*/
+                strFoundUrl = this.CleanURL(m.Groups[1].Value);
+                if (!listFoundUrls.Contains(strFoundUrl))
+                    listFoundUrls.Add(strFoundUrl);
+                //}
+            }
+        }
+
+    }
+}
